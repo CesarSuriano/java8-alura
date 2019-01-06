b75ebf5902af741785ce9071067bd80a9cb91345
Cesar Brasil <cesaraugustokg6@hotmail.com>
2019-01-06 02:25:57 -0200

    começando aula 04

--- src/aula2/ThreadLambda.java
+++ src/aula2/ThreadLambda.java
@@ -1,5 +1,14 @@
 package aula2;
 
 public class ThreadLambda {
-
+	public static void main(String[] args) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Executando numa thread");
+			}
+		}).start();
+		
+		new Thread(() -> System.out.println("Executando thread com lambda")).start();
+	}
 }
---
+++ src/aula3/OrdenaStrings.java
@@ -0,0 +1,29 @@
+package aula3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class OrdenaStrings {
+	public static <T> void main(String[] args) {
+
+		List<String> palavras = new ArrayList<String>();
+		palavras.add("Cesar");
+		palavras.add("TEste");
+		palavras.add("Teste2");
+
+		//isso
+		palavras.sort(Comparator.comparing(String::length));
+		
+		//é a mesma coisa que isso
+		Function<String, Integer> funcao = s -> s.length();
+		Comparator<String> comparador = Comparator.comparing(funcao);
+		
+		System.out.println(palavras);
+
+		palavras.forEach(System.out::println);
+	}
+}
